a9f63ae294ddbc98881be04b38529b1f59fdc280
susu <clwmrndl92@gmail.com>
2024-05-13 21:51:09 +0900

    [feat] 스테이트 추가 및 state machine 스크립트 분리

---
+++ Assets/Scripts/Character/State/HurtState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LineUpHeros
+{
+    // 피격 스테이트
+    public class HurtState : BaseState
+    {
+        protected Character _character;
+        
+        public HurtState(Character character) : base(character)
+        {
+            _character = character;
+        }
+
+        public override void OnEnterState(){}
+        public override void OnUpdateState(){}
+        public override void OnFixedUpdateState(){}
+        public override void OnExitState(){}
+    }
+}
---
+++ Assets/Scripts/Character/State/VictoryState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LineUpHeros
+{
+    // 승리 스테이트
+    public class VictoryState : BaseState
+    {
+        protected Character _character;
+        
+        public VictoryState(Character character) : base(character)
+        {
+            _character = character;
+        }
+
+        public override void OnEnterState(){}
+        public override void OnUpdateState(){}
+        public override void OnFixedUpdateState(){}
+        public override void OnExitState(){}
+    }
+}
